cc9502bf9aef5e71761644eabeb08c3125e2c268
jinhoKong <tpdleps@gmail.com>
2017-10-14 16:37:14 +0900

    Init gh-pages

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie.js';
+
+
+
+//Update componentWillReceiveProps() -> shouldComponentUpdate()->componentWillUpdate()->render()->componentDidUpdate()
+//Render componentWillMount() -> render() -> componentDidMount()
+class App extends Component {
+  state={}
+  componentDidMount(){
+    this._getMovies();
+  }
+   _getMovies=async()=>{
+      const movies=await this._callAPI()
+      this.setState({
+        movies
+      })    
+  }
+  _callAPI=()=>{
+    return fetch("https://yts.ag/api/v2/list_movies.json?sort_by=download_count")
+    .then(potato=>potato.json())
+    .then(json=> json.data.movies)
+    .catch(err=>console.log(err))
+  }
+_renderMovies=()=>{
+  const movies=this.state.movies.map(movie=>{
+    return <Movie 
+    title={movie.title_english} 
+    poster={movie.large_cover_image} 
+    key={movie.id} 
+    genres={movie.genres}
+    synopsis={movie.synopsis}
+    />
+  })
+  return movies
+}
+  render() {
+    return (
+      <div className={ this.state.movies ? "App" : "App-loading"}>
+        {this.state.movies ? this._renderMovies():'Loding'}
+      </div>
+    );
+  }
+}
+
+export default App;
