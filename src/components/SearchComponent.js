3a3fdd93c90717130facb241e8db4bd92de2622f
jinhoKong <tpdleps@gmail.com>
2017-11-30 13:05:53 +0900

    img change

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,22 +1,46 @@
 import React, { Component, PropTypes } from 'react';
+import '../css/SearchComponent.css'
 const propTypes = {
 };
 const defaultProps = {
 };
 class SearchComponent extends Component {
-    state={
-        value:''
-    }
     constructor(props) {
         super(props);
+    }
+    state={
+        value:''
+    };
+    handleSearch(){
+        this.props.onClear();
+        this.props.onSearch(this.state.value.trim());
+    }
+    onSearch
+    clearSearch() {
+        this.setState({
+          value: ''
+        });
+        this.input.focus();
+        this.props.onClear();
     }
    render() {
       return(
          <div id="SearchComponent">
             <input
                 type="text"
+                size='20'
                 placeholder="Search Movie"
+                value={this.state.value}
             />
+            <button 
+                className="clear-button"
+                onClick={this.clearSearch}
+            />
+            <button 
+            className="submit-button"
+            onClick={this.handleSearch}
+            />
+
          </div>
     );
     }
