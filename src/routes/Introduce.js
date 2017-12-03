147510a76844dfe2289818bf447d7a47e2d49afc
jinhoKong <tpdleps@gmail.com>
2017-12-04 02:17:58 +0900

    delete search from introduce

--- src/routes/Introduce.js
+++ src/routes/Introduce.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../css/Introduce.css'
-import SearchComponent from '../components/SearchComponent'
+// import SearchComponent from '../components/SearchComponent'
 const Introduce = () => {
     return (
         <div>
-            <SearchComponent/>
+        
         </div>
     );
 };
