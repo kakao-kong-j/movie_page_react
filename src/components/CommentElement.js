c3e507cffccea752add3fbf348137051812d28c6
jinhoKong <tpdleps@gmail.com>
2017-12-07 00:29:05 +0900

    comment active without design,login

--- src/components/CommentElement.js
+++ src/components/CommentElement.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import {database} from '../script/firebase' 
+
 class CommentElemnet extends Component {
     constructor(props) {
         super(props);
         this.state={
             id:this.props.id,
         }
-        this.test = this.test.bind(this);
     }
-    test(){
-        const a= database.ref().child('Comment').child(this.state.id);
-        a.on(
-            'value',snap=>console.log(snap.val())
-        )
-    }
+    
     render() {
         return (
             <div>
-            <button 
-            className="comment-submit-button"
-            onClick={this.test}
-        >
-            Submit!!!!!!!!!!!!!!
-        </button>
+            {this.props.id}
+            <br/>
+            <br/>
+            {this.props.email}
+            <br/>
+            {this.props.time}
+            <br/>
+            {this.props.rating}
+            <br/>
+            {this.props.commentvalue}
+            <br/>
             </div>
         );
     }
--- src/components/MovieCastDetail.js
+++ src/components/MovieCastDetail.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import img from "../img/man.png";
 class MovieCastDetail extends Component {
     render() {
         return (
             <div>
                 <span className="Movie__CastingDetail">  
                     <div className="Casting_Img">
-                    <img src={this.props.url_small_image} title={this.props.name} alt={this.props.name} className="Casting__Img"/>
+                        <img 
+                            src={ (this.props.url_small_image) ? this.props.url_small_image : img}
+                            title={this.props.name} alt={this.props.name} className="Casting__Img"/>
                     </div>
                     <div className="Casting_Name">
                         Name: {this.props.name}
