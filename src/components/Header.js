fc179393c0283df98b8f75b9eddfc3c4293dc30c
jinhoKong <tpdleps@gmail.com>
2017-12-10 23:38:49 +0900

    Header login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../css/Header.css';
-
-const Header = () => {
+import {firebaseAuth} from '../script/firebase'
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.authCheck=this.authCheck.bind(this)
+        this.state={
+            authed:false
+        }
+    }
+    componentWillMount()
+    {
+        this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+            if (user) {
+              this.setState({
+                authed: true,
+                loading: false,
+              })
+            } else {
+              this.setState({
+                authed: false,
+                loading: false
+              })
+            }
+          })  
+    }
+    
+    authCheck()
+    {
+    if(this.state.authed)
+    {
+        return   (
+            <NavLink to="/login" className="flex-sm-fill text-sm-center nav-link px-8 float-right align-self-center" activeClassName="nav-link active">Profile</NavLink>
+        )
+    }
+    else{
+        <NavLink to="/login" className="flex-sm-fill text-sm-center nav-link px-5 float-right align-self-center" activeClassName="nav-link active">Login</NavLink>
+    }
+}
+   render() {
     return (
         <div>
             <div className="nav nav-pills flex-column lg flex-sm-row bg-dark">
@@ -10,10 +47,10 @@ const Header = () => {
                 <NavLink exact to="/" className="flex-sm-fill text-sm-center nav-link px-5 float-right align-self-center" activeClassName="nav-link active">Introduce</NavLink>
                 <NavLink to="/MovieList" className="flex-sm-fill text-sm-center nav-link px-5 float-right align-self-center" activeClassName="nav-link active">MovieList</NavLink>
                 <NavLink to="/board" className="flex-sm-fill text-sm-center nav-link px-5 float-right align-self-center" activeClassName="nav-link active">Board</NavLink>
-                <NavLink to="/login" className="flex-sm-fill text-sm-center nav-link px-5 float-right align-self-center" activeClassName="nav-link active">Login</NavLink>
+                {this.authCheck()}
             </div>
         </div>
     );
-};
-
+    }
+}
 export default Header;
